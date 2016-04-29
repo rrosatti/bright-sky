21a34d433d310b6eb50b1dff96f93bf87f037041
Rodrigo <rodrigo_rosatti@hotmail.com>
2016-04-29 15:21:43 -0300

    add WeatherFragment class

---
+++ app/src/main/java/com/example/rodri/brightsky/ui/CityPreference.java
@@ -0,0 +1,29 @@
+package com.example.rodri.brightsky.ui;
+
+import android.app.Activity;
+import android.content.SharedPreferences;
+
+/**
+ * Created by rodri on 4/29/2016.
+ *
+ *  This class will be used to save the user's chosen city,
+ *  so he/she won't need to set the city every time when the app starts
+ *
+ */
+public class CityPreference {
+
+    SharedPreferences sharedPreferences;
+
+    public CityPreference(Activity activity) {
+        sharedPreferences = activity.getPreferences(Activity.MODE_PRIVATE);
+    }
+
+    /** Set São Paulo as default city if the user hasn't chosen a city */
+    public String getCity() {
+        return sharedPreferences.getString("city", "São Paulo, BR");
+    }
+
+    void setCity(String city) {
+        sharedPreferences.edit().putString("city", city).commit();
+    }
+}
---
+++ app/src/main/java/com/example/rodri/brightsky/ui/fragment/WeatherFragment.java
@@ -0,0 +1,92 @@
+package com.example.rodri.brightsky.ui.fragment;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.rodri.brightsky.R;
+import com.example.rodri.brightsky.ui.CityPreference;
+import com.example.rodri.brightsky.ui.json.RemoteFetch;
+
+import android.os.Handler;
+import android.widget.Toast;
+
+import org.json.JSONObject;
+
+import java.util.Locale;
+
+/**
+ * Created by rodri on 4/29/2016.
+ */
+public class WeatherFragment extends Fragment {
+
+    TextView txtCurrentWeather;
+    TextView txtCityAndCountry;
+    /** handler will be used to use a separate Thread to fetch data from the OpenWeatherMap API (asynchronously) */
+    Handler handler;
+
+    public WeatherFragment() {
+        handler = new Handler();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_weather, container, false);
+        txtCurrentWeather = (TextView) rootView.findViewById(R.id.txtCurrentWeather);
+        txtCityAndCountry = (TextView) rootView.findViewById(R.id.txtCityAndCountry);
+
+        return rootView;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        updateWeatherData(new CityPreference(getActivity()).getCity());
+    }
+
+    private void updateWeatherData(final String city) {
+        new Thread() {
+            public void run() {
+                final JSONObject json = RemoteFetch.getJSON(getActivity(), city);
+                /** if json = null we show an error message, else we start renderWeather() */
+                if (json == null) {
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(getActivity(), R.string.city_not_found, Toast.LENGTH_LONG).show();
+                        }
+                    });
+                } else {
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            renderWeather(json);
+                        }
+                    });
+                }
+            }
+        }.start();
+    }
+
+    private void renderWeather(JSONObject json) {
+        try {
+            txtCityAndCountry.setText(json.getString("name").toUpperCase(Locale.US) +
+            ", " + json.getJSONObject("sys").getString("country"));
+
+            JSONObject main = json.getJSONObject("main");
+
+            txtCurrentWeather.setText(String.format("%.2f", main.getDouble("temp")) + " ºC");
+        } catch (Exception e) {
+            Log.e("BrightSky", "One or more fields not found in the JSON data.");
+        }
+    }
+
+    public void changeCity(String city) {
+        updateWeatherData(city);
+    }
+}
